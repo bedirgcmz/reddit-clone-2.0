726747cbf4b1f5b60729a968425b2f7c7ef8283b
bedirgcmz <bdrgcmz@gmail.com>
2024-12-16 10:55:44 +0100

    The project was moved to the new repo due to the error of node_modules being added to the previous repo by mistake.

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import 'dotenv/config'
+
+import { postRouter } from './routes/post'
+import { authRouter } from './routes/auth'
+import { profileRouter } from './routes/profile'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(postRouter)
+app.use(authRouter)
+app.use(profileRouter)
+
+mongoose.connect(process.env.DB_URL!).then(() => {
+  const port = process.env.PORT || '8080'
+  app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}/`)
+  })
+})
